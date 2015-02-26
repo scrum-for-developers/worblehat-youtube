ef72fb2a8045612054324845609e8b99bc3298e3
tobiasflohre <tobias.flohre@codecentric.de>
2015-02-26 10:01:48 +0100

    MessageBundle

--- worblehat-web/src/main/java/de/codecentric/config/ApplicationWebConfiguration.java
+++ worblehat-web/src/main/java/de/codecentric/config/ApplicationWebConfiguration.java
@@ -4,6 +4,7 @@ import java.util.Locale;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
@@ -22,6 +23,14 @@ public class ApplicationWebConfiguration extends WebMvcConfigurerAdapter {
         SessionLocaleResolver slr = new SessionLocaleResolver();
         slr.setDefaultLocale(Locale.US);
         return slr;
+    }
+    
+    @Bean
+    public ReloadableResourceBundleMessageSource messageSource(){
+    	ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+    	messageSource.setUseCodeAsDefaultMessage(true);
+    	messageSource.setBasename("classpath:messages");
+    	return messageSource;
     }
     
 }
