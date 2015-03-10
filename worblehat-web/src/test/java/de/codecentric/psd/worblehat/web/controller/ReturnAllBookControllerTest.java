a6707793a76b0aa0040c8c6477bc13228a961f40
Benedikt Ritter <beneritter@gmail.com>
2015-03-10 21:07:05 +0100

    Use constructor injection in ReturnAllBookController

--- worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/ReturnAllBookControllerTest.java
+++ worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/ReturnAllBookControllerTest.java
@@ -4,37 +4,32 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import de.codecentric.psd.worblehat.domain.BookService;
+import de.codecentric.psd.worblehat.web.command.ReturnAllBooksFormData;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 
-import de.codecentric.psd.worblehat.domain.BookService;
-import de.codecentric.psd.worblehat.web.command.ReturnAllBooksFormData;
-
 public class ReturnAllBookControllerTest {
 
-	@InjectMocks
 	private ReturnAllBookController returnAllBookController;
 
 	private ModelMap modelMap;
 
-	@Mock
 	private BookService bookService;
 
-	@Mock
 	private BindingResult mockBindingResult;
 
 	@Before
 	public void setUp() {
-		returnAllBookController = new ReturnAllBookController();
-		MockitoAnnotations.initMocks(this);
+		mockBindingResult = mock(BindingResult.class);
+		bookService = mock(BookService.class);
+		returnAllBookController = new ReturnAllBookController(bookService);
 		modelMap = new ModelMap();
 	}
 
@@ -51,7 +46,7 @@ public class ReturnAllBookControllerTest {
 		when(mockBindingResult.hasErrors()).thenReturn(false);
 		ReturnAllBooksFormData formData = new ReturnAllBooksFormData();
 		formData.setEmailAddress("email@email.de");
-		String page = returnAllBookController.returnAllBooks(modelMap,
+		String page = returnAllBookController.returnAllBooks(
 				formData, mockBindingResult);
 		assertThat(page, is("/home"));
 		verify(mockBindingResult).hasErrors();
@@ -61,7 +56,7 @@ public class ReturnAllBookControllerTest {
 	@Test
 	public void shouldNotReturnAllBook() {
 		when(mockBindingResult.hasErrors()).thenReturn(true);
-		String page = returnAllBookController.returnAllBooks(modelMap,
+		String page = returnAllBookController.returnAllBooks(
 				new ReturnAllBooksFormData(), mockBindingResult);
 		assertThat(page, is("/returnAllBooks"));
 		verify(mockBindingResult).hasErrors();
