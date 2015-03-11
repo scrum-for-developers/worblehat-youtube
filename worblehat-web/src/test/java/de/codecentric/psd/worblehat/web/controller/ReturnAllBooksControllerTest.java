ffaed0e51d6129677c84fe3b1fef9b43f4d6911b
Benedikt Ritter <beneritter@gmail.com>
2015-03-11 09:58:21 +0100

    Reanme ReturnAllBookController to ReturnAllBooksController

---
+++ worblehat-web/src/test/java/de/codecentric/psd/worblehat/web/controller/ReturnAllBooksControllerTest.java
@@ -0,0 +1,64 @@
+package de.codecentric.psd.worblehat.web.controller;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import de.codecentric.psd.worblehat.domain.BookService;
+import de.codecentric.psd.worblehat.web.command.ReturnAllBooksFormData;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+
+public class ReturnAllBooksControllerTest {
+
+	private ReturnAllBooksController controller;
+
+	private ModelMap modelMap;
+
+	private BookService bookService;
+
+	private BindingResult mockBindingResult;
+
+	@Before
+	public void setUp() {
+		mockBindingResult = mock(BindingResult.class);
+		bookService = mock(BookService.class);
+		controller = new ReturnAllBooksController(bookService);
+		modelMap = new ModelMap();
+	}
+
+	@Test
+	public void shouldPrepareView() {
+		controller.prepareView(modelMap);
+		ReturnAllBooksFormData object = (ReturnAllBooksFormData) modelMap
+				.get("returnAllBookFormData");
+		assertThat(object, not(nullValue()));
+	}
+
+	@Test
+	public void shouldReturnAllBook() {
+		when(mockBindingResult.hasErrors()).thenReturn(false);
+		ReturnAllBooksFormData formData = new ReturnAllBooksFormData();
+		formData.setEmailAddress("email@email.de");
+		String page = controller.returnAllBooks(
+				formData, mockBindingResult);
+		assertThat(page, is("/home"));
+		verify(mockBindingResult).hasErrors();
+		verify(bookService).returnAllBooksByBorrower("email@email.de");
+	}
+
+	@Test
+	public void shouldNotReturnAllBook() {
+		when(mockBindingResult.hasErrors()).thenReturn(true);
+		String page = controller.returnAllBooks(
+				new ReturnAllBooksFormData(), mockBindingResult);
+		assertThat(page, is("/returnAllBooks"));
+		verify(mockBindingResult).hasErrors();
+	}
+}
