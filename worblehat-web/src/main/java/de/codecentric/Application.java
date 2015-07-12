5b685da499d18ab994726fd3bc8a276286f0c0d0
Benedikt Ritter <beneritter@gmail.com>
2015-07-12 13:54:17 +0200

    Remove unnecessary configuration

--- worblehat-web/src/main/java/de/codecentric/Application.java
+++ worblehat-web/src/main/java/de/codecentric/Application.java
@@ -2,10 +2,9 @@ package de.codecentric;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
+public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
