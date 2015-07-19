586628ea7e2d77593cf7e65d1ce4e91fd1cab392
Benedikt Ritter <beneritter@gmail.com>
2015-07-19 21:25:43 +0200

    Add helper script for creating a docker based worblehat mysql data base

---
+++ worblehat-web/docker-db.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+  
+docker run --detach \
+  --name worblehat-db \
+  --env MYSQL_ROOT_PASSWORD=root \
+  --env MYSQL_USER=worblehat \
+  --env MYSQL_PASSWORD=worblehat \
+  --env MYSQL_DATABASE=worblehat \
+  --publish 3306:3306 \
+  mysql:5.6.25
+
