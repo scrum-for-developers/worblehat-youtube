b9896612b1f61fb7d579e5b38c7509630547a3ad
Benedikt Ritter <beneritter@gmail.com>
2016-08-21 13:23:10 +0200

    Rename BookServiceTest to StandardBookServiceTest, since it tests the StandardBookService.

--- worblehat-domain/src/test/java/de/codecentric/psd/worblehat/domain/StandardBookServiceTest.java
+++ worblehat-domain/src/test/java/de/codecentric/psd/worblehat/domain/StandardBookServiceTest.java
@@ -1,23 +1,22 @@
 package de.codecentric.psd.worblehat.domain;
 
-import de.codecentric.psd.worblehat.domain.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Spy;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Date;
-import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-public class BookServiceTest {
+public class StandardBookServiceTest {
 
 	private BorrowingRepository borrowingRepository;
 
