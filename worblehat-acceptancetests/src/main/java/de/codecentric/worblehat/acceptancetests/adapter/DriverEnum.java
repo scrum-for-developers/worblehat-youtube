f85699b4bcc30b83a4c7a4687b96c226b16c5a55
Andreas Houben <andreas.houben@codecentric.de>
2017-03-19 17:23:36 +0100

    cleanup. Automatically download binaries for ATs

--- worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/DriverEnum.java
+++ worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/DriverEnum.java
@@ -1,20 +1,38 @@
 package de.codecentric.worblehat.acceptancetests.adapter;
 
 import org.apache.commons.lang.SystemUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.concurrent.TimeUnit;
 
 public enum DriverEnum {
 
     CHROME, PHANTOMJS;
 
     private Logger LOGGER = LoggerFactory.getLogger(DriverEnum.class);
-    private static String DRIVER_PATH = "src/main/resources/webdriver/";
-    private static String CHROMEDRIVER_MAC = "chromedriver_mac64";
-    private static String CHROMEDRIVER_WIN = "chromedriver.exe";
-    private static String CHROMEDRIVER_LINUX = "chromedriver_linux64";
+
+    private static String DRIVER_PATH =
+            "target/binaries/";
+
+    private static String CHROMEDRIVER_MAC = "osx/googlechrome/64bit/chromedriver";
+
+    private static String CHROMEDRIVER_WIN = "windows/googlechrome/64bit/chromedriver.exe";
+
+    private static String CHROMEDRIVER_LINUX = "linux/googlechrome/64bit/chromedriver";
+
+    private static String PHANTOMJSDRIVER_MAC = "osx/phantomjs/64bit/phantomjs";
+
+    private static String PHANTOMJSDRIVER_WIN = "windows/phantomjs/64bit/phantomjs.exe";
+
+    private static String PHANTOMJSDRIVER_LINUX = "linux/phantomjs/64bit/phantomjs";
 
     WebDriver getDriver() throws Exception {
         if (name().equals(CHROME.name())) {
@@ -23,8 +41,25 @@ public enum DriverEnum {
         return createPhantomJSDriver();
     }
 
-    private WebDriver createPhantomJSDriver() {
-        return null;
+    private WebDriver createPhantomJSDriver() throws Exception {
+        StringBuilder sb = new StringBuilder(DRIVER_PATH);
+        if (SystemUtils.IS_OS_MAC) {
+            sb.append(PHANTOMJSDRIVER_MAC);
+        } else if (SystemUtils.IS_OS_WINDOWS) {
+            sb.append(PHANTOMJSDRIVER_WIN);
+        } else if (SystemUtils.IS_OS_LINUX) {
+            sb.append(PHANTOMJSDRIVER_LINUX);
+        } else {
+            throw new Exception("Unsupported operation system for chromedriver");
+        }
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(
+                PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
+                sb.toString());
+        WebDriver driver = new PhantomJSDriver(caps);
+        driver.manage().window().setSize(new Dimension(1280, 800));
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        return driver;
     }
 
     private WebDriver createChromeDriver() throws Exception {
@@ -38,10 +73,11 @@ public enum DriverEnum {
         } else {
             throw new Exception("Unsupported operation system for chromedriver");
         }
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(ChromeDriverService.CHROME_DRIVER_EXE_PROPERTY,
+                sb.toString());
         System.setProperty("webdriver.chrome.driver", sb.toString());
-        WebDriver driver = new ChromeDriver();
-        assertNotNull(driver);
-        return new ChromeDriver();
+        return new ChromeDriver(caps);
     }
 
 }
--- worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/SeleniumAdapter.java
+++ worblehat-acceptancetests/src/main/java/de/codecentric/worblehat/acceptancetests/adapter/SeleniumAdapter.java
@@ -10,13 +10,10 @@ import org.jbehave.core.annotations.BeforeStories;
 import org.jbehave.core.annotations.ScenarioType;
 import org.joda.time.LocalDateTime;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebDriver.Options;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -25,7 +22,6 @@ import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 /**
  * Itegrates Selenium into the tests.
@@ -34,41 +30,27 @@ import java.util.concurrent.TimeUnit;
 public class SeleniumAdapter {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SeleniumAdapter.class);
-    private WebDriver driver;
-    private String folderName;
 
-    public static final String USER_AGENT =
-            "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36";
+    private WebDriver driver;
+
+    private String folderName;
 
     @BeforeStories
     public void initSelenium() {
         String seleniumProvider = Config.getEnvironment();
-        try{
+        try {
             if (seleniumProvider.equalsIgnoreCase("local")) {
                 driver = DriverEnum.CHROME.getDriver();
             } else {
-                driver = new PhantomJSDriver();
-                driver.manage().window().setSize(new Dimension(1280, 800));
-                driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+                driver = DriverEnum.PHANTOMJS.getDriver();
             }
-        } catch (Exception e){
+        } catch (Exception e) {
             LOGGER.error("Error initializing Webdriver", e);
         }
-
 
         folderName = LocalDateTime.now().toString("yyyy-MM-dd HH:mm").concat("/");
         folderName = "target/screenshots/".concat(folderName);
         new File(folderName).mkdirs();
-
-        // // Find the text input element by its name
-        // WebElement element = driver.findElement(By.name("q"));
-        //
-        // // Enter something to search for
-        // element.sendKeys("Cheese!");
-        //
-        // // Now submit the form. WebDriver will find the form for us from the
-        // // element
-        // element.submit();
 
     }
 
@@ -109,10 +91,8 @@ public class SeleniumAdapter {
     }
 
     @AfterScenario(uponType = ScenarioType.EXAMPLE)
-    // equivalent to @AfterScenario(uponOutcome=AfterScenario.Outcome.ANY)
     public void afterAnyScenario() {
-        Options driverOptions = driver.manage();
-        driverOptions.deleteAllCookies();
+        driver.manage().deleteAllCookies();
     }
 
     public String getTextFromElement(PageElement pageElement) {
