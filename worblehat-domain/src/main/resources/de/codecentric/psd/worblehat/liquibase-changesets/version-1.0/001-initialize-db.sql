1683a0dcb47a66bd80dbc1a2d0508377fef760e0
Benedikt Ritter <benedikt.ritter@codecentric.de>
2018-07-23 16:10:34 +0200

    Migrationsskripte für PostgreSQL angepasst.
    PostgreSQL spezifischen Datentyp BIGSERIAl verwendet. Dieser soll laut
    Dokumentation für Primärschlüssel verwendet werden:
      https://www.postgresql.org/docs/8.1/static/datatype.html#DATATYPE-SERIAL
    
    Doppelte Anführungszeichen durch einfache Anführungszeichen ersetzt.
    
    Entfernen des UNIQUE Constraints auf der isbn Spalte entsprechend
    angepasst. Den Namen des Constraints musste ich dafür in der Tabelle
    table_constraints im information_schema nachschauen.

--- worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.0/001-initialize-db.sql
+++ worblehat-domain/src/main/resources/de/codecentric/psd/worblehat/liquibase-changesets/version-1.0/001-initialize-db.sql
@@ -2,7 +2,7 @@
 
 -- changeset action:create_table_books
 CREATE TABLE book (
-  id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id BIGSERIAL PRIMARY KEY,
   title VARCHAR(255),
   author VARCHAR(255),
   edition VARCHAR(255),
@@ -13,7 +13,7 @@ CREATE TABLE book (
 
 -- changeset action:create_table_borrowing
 CREATE TABLE borrowing(
-  id BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  id BIGSERIAL PRIMARY KEY,
   borrow_date DATE,
   borrower_email_address VARCHAR(255),
   borrowed_book_id BIGINT,
@@ -25,9 +25,9 @@ CREATE TABLE borrowing(
 -- changeset action:insert_demo_data
 INSERT INTO book(title, author, edition, isbn, year_of_publication)
 VALUES
-  ("Harry Potter and the Philisopher's Stone", "J.K. Rowling", "", "0747532699", 1997),
-  ("Harry Potter and the Prisoner of Azkaban", "J.K. Rowling", "", "0747542155", 1999),
-  ("Harry Potter and the Goblet of Fire", "J.K. Rowling", "", "074754624X", 2000),
-  ("Harry Potter and the Order of the Phoenix", "J.K. Rowling", "", "0747551006", 2003),
-  ("Harry Potter and the Half-Blood Prince", "J.K. Rowling", "", "0747581088", 2005),
-  ("Harry Potter and the Deathly Hallows", "J.K. Rowling", "", "0545010225", 2007);
+  ('Harry Potter and the Philisopher''s Stone', 'J.K. Rowling', '', '0747532699', 1997),
+  ('Harry Potter and the Prisoner of Azkaban', 'J.K. Rowling', '', '0747542155', 1999),
+  ('Harry Potter and the Goblet of Fire', 'J.K. Rowling', '', '074754624X', 2000),
+  ('Harry Potter and the Order of the Phoenix', 'J.K. Rowling', '', '0747551006', 2003),
+  ('Harry Potter and the Half-Blood Prince', 'J.K. Rowling', '', '0747581088', 2005),
+  ('Harry Potter and the Deathly Hallows', 'J.K. Rowling', '', '0545010225', 2007);
