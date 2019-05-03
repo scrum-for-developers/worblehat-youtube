6eea571e73d637bb6fde30e6aa72a99f9f5f08e0
Kevin Wittek <kevin.wittek@codecentric.de>
2019-05-03 18:22:02 +0200

    Add WorblehatDev for local development

--- worblehat-web/src/test/java/de/codecentric/psd/WorblehatSystemIntegrationTest.java
+++ worblehat-web/src/test/java/de/codecentric/psd/WorblehatSystemIntegrationTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@SpringBootTest()
 @ExtendWith(SpringExtension.class)
 class WorblehatSystemIntegrationTest {
 
