scrum-for-developers/worblehat

Replace JBehave with Cucumber

worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/acceptancetests/step/page/BorrowBook.java
@@ -1,0 +1,55 @@
+package de.codecentric.psd.worblehat.acceptancetests.step.page;
+
+import de.codecentric.psd.worblehat.acceptancetests.adapter.SeleniumAdapter;
+import de.codecentric.psd.worblehat.acceptancetests.adapter.wrapper.Page;
+import de.codecentric.psd.worblehat.acceptancetests.adapter.wrapper.PageElement;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BorrowBook {
+	
+	private SeleniumAdapter seleniumAdapter;
+
+	@Autowired
+	public BorrowBook(SeleniumAdapter seleniumAdapter) {
+		this.seleniumAdapter = seleniumAdapter;
+	}
+	
+	// *******************
+	// *** G I V E N *****
+	// *******************
+
+	// *****************
+	// *** W H E N *****s
+	// *****************
+
+	@When("user {string} borrows the book {string}")
+	public void whenUseruserBorrowsTheBookisbn(String user, String isbn){
+		seleniumAdapter.gotoPage(Page.BORROWBOOK);
+		seleniumAdapter.typeIntoField("email", user);
+		seleniumAdapter.typeIntoField("isbn", isbn);
+		seleniumAdapter.clickOnPageElement(PageElement.BORROWBOOKBUTTON);
+	}
+	
+	// *****************
+	// *** T H E N *****
+	// *****************
+
+	@Then("I get an error message {string} when the borrower {string} tries to borrow the book with isbn {string} again")
+	public void whenBorrowerBorrowsBorrowedBookShowErrorMessage(String message,
+																String borrower,
+																String isbn){
+		seleniumAdapter.gotoPage(Page.BORROWBOOK);
+		seleniumAdapter.typeIntoField("email", borrower);
+		seleniumAdapter.typeIntoField("isbn", isbn);
+		seleniumAdapter.clickOnPageElement(PageElement.BORROWBOOKBUTTON);
+		String errorMessage = seleniumAdapter.getTextFromElement(PageElement.ISBNERROR);
+		assertThat(errorMessage, is(message));
+	}
+
+
+}

scrum-for-developers/worblehat-youtube

worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/acceptancetests/step/page/BorrowBook.java
@@ -1,0 +1,55 @@
+package de.codecentric.psd.worblehat.acceptancetests.step.page;
+
+import de.codecentric.psd.worblehat.acceptancetests.adapter.SeleniumAdapter;
+import de.codecentric.psd.worblehat.acceptancetests.adapter.wrapper.Page;
+import de.codecentric.psd.worblehat.acceptancetests.adapter.wrapper.PageElement;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class BorrowBook {
+
+	private SeleniumAdapter seleniumAdapter;
+
+	@Autowired
+	public BorrowBook(SeleniumAdapter seleniumAdapter) {
+		this.seleniumAdapter = seleniumAdapter;
+	}
+
+	// *******************
+	// *** G I V E N *****
+	// *******************
+
+	// *****************
+	// *** W H E N *****s
+	// *****************
+
+	@When("user {string} borrows the book {string}")
+	public void whenUseruserBorrowsTheBookisbn(String user, String isbn){
+		seleniumAdapter.gotoPage(Page.BORROWBOOK);
+		seleniumAdapter.typeIntoField("email", user);
+		seleniumAdapter.typeIntoField("isbn", isbn);
+		seleniumAdapter.clickOnPageElementById(PageElement.BORROWBOOKBUTTON);
+	}
+
+	// *****************
+	// *** T H E N *****
+	// *****************
+
+	@Then("I get an error message {string} when the borrower {string} tries to borrow the book with isbn {string} again")
+	public void whenBorrowerBorrowsBorrowedBookShowErrorMessage(String message,
+																String borrower,
+																String isbn){
+		seleniumAdapter.gotoPage(Page.BORROWBOOK);
+		seleniumAdapter.typeIntoField("email", borrower);
+		seleniumAdapter.typeIntoField("isbn", isbn);
+		seleniumAdapter.clickOnPageElementById(PageElement.BORROWBOOKBUTTON);
+		String errorMessage = seleniumAdapter.getTextFromElement(PageElement.ISBN_ERROR);
+		assertThat(errorMessage, is(message));
+	}
+
+
+}