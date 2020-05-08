e8f26dcbad4cc9820cb5ee1749f7796b30b50086
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-05-08 16:50:07 +0200

    build worblehat-domain with gradle

---
+++ worblehat-domain/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+  `java-library`
+}
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(platform("org.springframework.boot:spring-boot-dependencies:2.1.4.RELEASE"))
+  implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+  runtimeOnly("org.postgresql:postgresql")
+  runtimeOnly("org.liquibase:liquibase-core")
+
+  implementation("com.google.code.findbugs:jsr305:3.0.2")
+  implementation("com.google.guava:guava:29.0-jre")
+
+  compileOnly("org.projectlombok:lombok:1.18.12")
+  annotationProcessor("org.projectlombok:lombok:1.18.12")
+
+  testImplementation("org.mockito:mockito-core")
+  testImplementation("com.github.npathai:hamcrest-optional:2.0.0")
+  testImplementation("org.junit.jupiter:junit-jupiter-api")
+  testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+}
+
+tasks {
+  test {
+    useJUnitPlatform()
+  }
+}
