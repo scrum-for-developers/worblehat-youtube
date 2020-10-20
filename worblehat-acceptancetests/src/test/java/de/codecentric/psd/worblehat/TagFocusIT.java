e98e9c07e6a61b2eb4370d51fd1daecdcec512e8
Andreas Ebbert-Karroum <andreas@karroum.de>
2020-10-20 10:30:32 +0200

    add cucumber junit runner for @focus tagged tests

---
+++ worblehat-acceptancetests/src/test/java/de/codecentric/psd/worblehat/TagFocusIT.java
@@ -0,0 +1,24 @@
+
+package de.codecentric.psd.worblehat;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    // run dedicated feature files
+    // features = {"src/test/resources/de/codecentric/psd/worblehat/features/book/Remove Book.feature"},
+    // run feature identified by a tag expression
+    tags = "@Focus",
+    strict = true,
+    stepNotifications = true,
+    plugin = {
+      "pretty",
+      "html:target/cucumber.html",
+      "junit:target/cucumber.xml",
+      "json:target/cucumber-report.json"
+    },
+    publish = false)
+public class TagFocusIT {
+}
